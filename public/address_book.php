60392fc819de727003b8160186ecabecfc662951
Greg Vallejo <greg.vallejo@gmail.com>
2014-06-02 18:24:52 -0500

    Beginning of assignment, used PHP to save CSV file of entries. Needs refactoring for other steps.
    "Open the CSV file in a spreadsheet program or text editor and verify the contents are what you expect after adding some entries.
    
    Refactor your code to use functions where applicable."

---
+++ public/address_book.php
@@ -0,0 +1,92 @@
+<?php
+	//initialize address book
+	$address_book=[
+			['Greg Vallejo', '4527 Pinehurst Mesa', 'San Antonio', 'TX', '78247', '210-288-6520'],
+			['Codeup', '112 E. Pecan St', 'San Antonio', 'TX', '78205'],
+			['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
+		    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
+		    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
+		];
+	//initialize file location
+	$new_address = [];
+	$filename = "data/adr_bk.csv";
+	//writing csv file
+	function write_csv($bigArray, $filename) {
+		if (is_writeable($filename)) {
+			$handle = fopen($filename, 'w');
+			foreach ($bigArray as $fields) {
+				fputcsv($handle, $fields);
+			}
+			fclose($handle);
+		}
+	}
+	//checking if required fields are entered; if not, display error msg
+	if(!empty($_POST['name']) && !empty($_POST['streetAddress']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zipCode'])) {
+		foreach ($_POST as $key => $value) {
+			$new_address[] = $value;
+		} 
+	} else {
+		foreach ($_POST as $key => $value) {
+			if (empty($value)) {
+				echo $key . " is empty, please enter.\n" ;
+			}
+		}
+	}
+	array_push($address_book, $new_address);
+	write_csv($address_book, $filename);
+		
+
+		
+	var_dump($address_book);		
+?>
+<html>
+<title>Address Book</title>
+<head>
+</head>
+<body>
+<h1> ADDRESS BOOK </h1>
+	<table>
+	<tr>
+		<th>Name</th>
+		<th>Address</th>
+		<th>City</th>
+		<th>State</th>
+		<th>ZIP</th>
+		<th>Phone</th>
+	</tr>
+	<? foreach ($address_book as $fields): ?>
+		<tr>
+		<? foreach($fields as $value): ?>
+			<td> <?= htmlspecialchars(strip_tags($value)); ?> </td>
+		<?endforeach; ?>
+		</tr>
+	<?endforeach; ?>
+		
+
+	</tr>
+	</table>
+ 	<!-- user inputs address information -->
+	<form method='POST' action='address_book.php'>
+		<label for 'name'>Name*</label>
+		<input id='name' name='name' type='text' placeholder='Enter Name'>
+	
+	<br>
+		<label for 'streetAddress'>Address*</label>
+		<input id='streetAddress' name='streetAddress' type='text' placeholder='Street Address'>
+	<br>
+		<label for 'city'>City*</label>
+		<input id='city' name='city' type ='text' placeholder='City'>
+	<br>
+		<label for 'state'>State*</label>
+		<input id='state' name='state' type='text' placeholder='State'>
+	<br>
+		<label for 'zipCode'>ZIP Code*</label>
+		<input id='zipCode' name='zipCode' type='text' placeholder='ZIP Code'>
+	<br>
+		<label for 'phone'>Phone</label>
+		<input id='phone' name='phone' type='text' placeholder='Phone'>
+	<br>
+	<input type='SUBMIT'></input>
+	</form>	
+</body>
+</html>
