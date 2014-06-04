7b78fb331a38349529d24965d5fe93067e550f03
Greg Vallejo <greg.vallejo@gmail.com>
2014-06-04 09:51:58 -0500

    Added destructor to observe behavior

--- public/address_book.php
+++ public/address_book.php
@@ -2,13 +2,17 @@
 	//initialize file location
 	$new_address = [];
 	$address_book = [];
-	$filename = 'data/adr_bk.csv';
 
 	class AddressDataStore {
 
 	    public $filename = '';
 
-	    function read_address_book()
+	    public function __construct($filename)
+	    {
+	    	$this->filename = $filename;
+	    }
+
+	    public function readAddressBook()
 	    {
 	        $handle = fopen($this->filename, 'r');
 			$address_book = [];
@@ -22,22 +26,28 @@
 			return $address_book;
 	    }
 
-	    function write_address_book($addresses_array) 
+	    public function writeAddressBook($addresses_array) 
 	    {
 	        if (is_writeable($this->filename)) {
 				$handle = fopen($this->filename, 'w');
-				foreach ($bigArray as $fields) {
+				foreach ($addresses_array as $fields) {
 					fputcsv($handle, $fields);
 				}
 				fclose($handle);
 			}
 	    }
 
+	    public function __destruct()
+		{
+			echo "Class Dis-missed!\n";
+		}	    
 	}
 
-	$ads = new AddressDataStore;
-	$ads->filename = 'data/adr_bk.csv';
-	$address_book = $ads->read_address_book();
+	$ads = new AddressDataStore("data/adr_bk.csv");
+
+	//$ads->filename = 'data/adr_bk.csv'; (obsolete due to constructor)
+	$address_book = $ads->readAddressBook();
+	
 
 	
 
@@ -47,6 +57,7 @@
 			$new_address[] = $value;
 		} 
 		array_push($address_book, $new_address);
+		$ads->writeAddressBook($address_book);
 	} else {
 		foreach ($_POST as $key => $value) {
 			if (empty($value)) {
@@ -58,8 +69,35 @@
 	//removing item if link is clicked
 	if (isset($_GET['id'])) {
             unset($address_book[$_GET['id']]);
-            write_csv($address_book, $filename);
+            $ads->writeAddressBook($address_book);
+            
     }
+
+   
+    // Verify there were uploaded files and no errors
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+
+	    if ($_FILES['file1']["type"] != "text/csv") {
+	        echo "ERROR: file must be in text/csv!";
+	    } else {
+	        // Set the destination directory for uploads
+	        // Grab the filename from the uploaded file by using basename
+	        $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+	        $uploadFilename = basename($_FILES['file1']['name']);
+	        // Create the saved filename using the file's original name and our upload directory
+	        $saved_filename = $upload_dir . $uploadFilename;
+	        // Move the file from the temp location to our uploads directory
+	        move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+	        // load the new todos
+	        // merge with existing list
+	        $ups = new AddressDataStore($saved_filename);
+	        $addresses_uploaded = $ups->readAddressBook();
+	        $address_book = array_merge($address_book, $addresses_uploaded);
+	        $ads->writeAddressBook($address_book);
+	    }
+	}
+	unset($filename);
 ?>
 <html>
 <title>Address Book</title>
@@ -113,5 +151,14 @@
 	<br>
 	<input type='SUBMIT'></input>
 	</form>	
+	<form method="POST" enctype="multipart/form-data" action="/address_book.php">
+           <p>
+               <label for="file1">File to upload: </label>
+               <input type="file" id="file1" name="file1">
+           </p>
+
+           <p><input type="submit" value="Upload"></p>
+
+    </form>
 </body>
 </html>
