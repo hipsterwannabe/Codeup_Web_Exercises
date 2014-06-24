468a17a61ea8aca57cc15e4869e806eb251a4376
Greg Vallejo <greg.vallejo@gmail.com>
2014-06-24 11:20:59 -0500

    added pagination to show four entries at a time

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,44 +1,75 @@
 <?php
 
-// Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'greg', 'quiero');
+function getOffset() {
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-// Tell PDO to throw exceptions on error
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
-
-function getParks($dbc) {
-    // Bring the $dbc variable into scope somehow
-	
-	$stmt = $dbc->query('SELECT * FROM national_parks');
-
-    $national_parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    return $national_parks;
+    return ($page - 1) * 4;
 }
 
+
+
+if (!empty($_GET)) {
+    $pageID = $_GET['page'];
+} else {
+    $pageID = 0;
+}
+
+
+$limit = 4;
+$offset = ($pageID * $limit);
+
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'greg', 'quiero');
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$parks = $dbc->query('SELECT * FROM national_parks LIMIT ' . $limit . ' OFFSET ' . ($pageID * $limit))->fetchAll(PDO::FETCH_ASSOC);
+
+$count = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+
+$numPages = floor($count / $limit);
+
+
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>National Parks Exercise</title>
+    <meta charset="utf-8" />
+    <title>National Parks</title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" />
+
+    <script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
 </head>
 <body>
-<table>
-	<th>#</th>
-	<th>Park</th>
-	<th>State</th>
-	<th>Date Established</th>
-	<th>Area, in acres</th>	
-	
-		<? foreach (getParks($dbc) as $info => $park) { ?>
-		<tr>
-        	<? foreach ($park as $key => $value) { ?>
+    <div class="container">
+        <h1>National Parks <small>Information</small> </h1>
+
+        <table class="table table-striped table-hover">
+            <tr>
+            	<th>#</th>
+                <th>Name</th>
+                <th>State</th>
+                <th>Date Established</th>
+                <th>Area in Acres</th>
+            </tr>
+
+            <?php foreach ($parks as $park): ?>
+                <tr>
+                    <? foreach ($park as $key => $value) { ?>
         		<?= "<td>" . $value . "</td>"; ?>
-        	<? } ?> 
-        </tr>	
-        <? };?>
-	
-</table>
+        		<? } ?> 
+                </tr>
+            <?php endforeach ?>
+        </table>
+        <ul class="pager">
+            <?  if ($pageID != 0) : ?>
+                    <li class="previous"><a href="?page=<?=$pageID - 1?>">&larr; Previous</a></li>
+            <? endif; ?>
+            <? if ($pageID < $numPages) : ?>
+                <li class="next"><a href="?page=<?= $pageID + 1 ?>">Next &rarr;</a></li>
+            <? endif; ?>
+        </ul>
+    </div>
 </body>
 </html>
